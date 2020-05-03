52abe256e56d405382812346c16f80de66e5a274
Abiola <>
2020-05-03 19:33:33 +0100

    Updates

---
+++ precache-manifest.819031b7edb2fa710ef7a60dc1136ed2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d95592c93b42257b5594600ccf0d508f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f9bb4ad9651bd537260",
+    "url": "/static/css/main.72b3b685.chunk.css"
+  },
+  {
+    "revision": "8c3e4d609581d079d237",
+    "url": "/static/js/2.0fc4546b.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/static/js/2.0fc4546b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f9bb4ad9651bd537260",
+    "url": "/static/js/main.6fa61dec.chunk.js"
+  },
+  {
+    "revision": "e0b2c71ef9dcda333e12",
+    "url": "/static/js/runtime-main.7e15714c.js"
+  },
+  {
+    "revision": "ab638a9c54a04bd513949ccca809a07b",
+    "url": "/static/media/covid-19-eu-cord.org.ab638a9c.jpg"
+  }
+]);
