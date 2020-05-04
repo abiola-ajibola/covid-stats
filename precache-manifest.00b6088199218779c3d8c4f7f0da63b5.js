c1f62660fa8e932f224de0e1e7ccfbb54f90da04
Abiola <>
2020-05-04 08:43:00 +0100

    Updates

---
+++ precache-manifest.00b6088199218779c3d8c4f7f0da63b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a51e336d76539c5e8ab2b50b03170062",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57070aec893fe31ef569",
+    "url": "/static/css/main.72b3b685.chunk.css"
+  },
+  {
+    "revision": "8c3e4d609581d079d237",
+    "url": "/static/js/2.0fc4546b.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/static/js/2.0fc4546b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57070aec893fe31ef569",
+    "url": "/static/js/main.1108acef.chunk.js"
+  },
+  {
+    "revision": "e0b2c71ef9dcda333e12",
+    "url": "/static/js/runtime-main.7e15714c.js"
+  },
+  {
+    "revision": "ab638a9c54a04bd513949ccca809a07b",
+    "url": "/static/media/covid-19-eu-cord.org.ab638a9c.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.abcbc4125a961ebeb071e1ae2c2d669f.js"
+  "/precache-manifest.00b6088199218779c3d8c4f7f0da63b5.js"
 );
 
 self.addEventListener('message', (event) => {
