13fc6bfae00055a7f9d2f1a32293a36b0510c558
Abiola <>
2020-05-05 02:55:59 +0100

    Updates

---
+++ precache-manifest.92204486896904ecfa8142d4df29ea3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d8fd77bf2411acfb6aaecc4838ecb55",
+    "url": "/covid-stats/index.html"
+  },
+  {
+    "revision": "f4da30fe6cd37c921bea",
+    "url": "/covid-stats/static/css/main.980efba8.chunk.css"
+  },
+  {
+    "revision": "8c3e4d609581d079d237",
+    "url": "/covid-stats/static/js/2.0fc4546b.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/covid-stats/static/js/2.0fc4546b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4da30fe6cd37c921bea",
+    "url": "/covid-stats/static/js/main.6b7e8d6e.chunk.js"
+  },
+  {
+    "revision": "6933ab3cd8467cdd7cd7",
+    "url": "/covid-stats/static/js/runtime-main.cea77864.js"
+  },
+  {
+    "revision": "ab638a9c54a04bd513949ccca809a07b",
+    "url": "/covid-stats/static/media/covid-19-eu-cord.org.ab638a9c.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid-stats/precache-manifest.dae95a2bc1647a8e8c9c6f9cf39f009d.js"
+  "/covid-stats/precache-manifest.92204486896904ecfa8142d4df29ea3c.js"
 );
 
 self.addEventListener('message', (event) => {
